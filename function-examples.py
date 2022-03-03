3c7d85a2db65a91c60c7960538cfb80677480dc6
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-03-03 11:33:42 -0500

    convert seconds

--- function-examples.py
+++ function-examples.py
@@ -7,3 +7,12 @@ def print_seconds(hours, minutes, seconds):
     print((hours*3600) + (minutes*60) + seconds)
 
 print_seconds(1, 2, 3) #3723
+
+def convert_seconds(seconds):
+    hours = seconds // 3600 #floor division(//). How many hours is in the give amount of seconds.
+    minutes = (seconds - hours * 3600) // 60 #how many minutes are there left when we subtract the hour
+    remaining_seconds = seconds - hours * 3600 - minutes * 60 # how many seconds remaine after subtrating minutes
+    return hours, minutes, remaining_seconds
+
+hours, minutes, seconds = convert_seconds(5000)
+print(hours, minutes, seconds) #1 23 20
