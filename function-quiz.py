a6946ff0a74d97a33bae2cb45f5a8a23063ad4c3
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-04-01 10:04:07 -0400

    function quiz

--- function-quiz.py
+++ function-quiz.py
@@ -22,3 +22,19 @@ my_trip_miles = 55
 my_trip_km = convert_distance(my_trip_miles)
 print("The distance in kilometers is " + str(my_trip_km))
 print("The round-trip in kilometers is " + str(my_trip_km * 2))
+
+################################################################
+
+# 2- This function compares two numbers and returns them
+# in increasing order.
+def order_numbers(number1, number2):
+	if number2 > number1:
+		return number1, number2
+	else:
+		return number2, number1
+
+# 1) Fill in the blanks so the print statement displays the result
+#    of the function call
+smaller, bigger = order_numbers(100, 99)
+print(smaller, bigger)
+
