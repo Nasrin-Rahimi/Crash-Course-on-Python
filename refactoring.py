ba95404f677cdd4ecb3f0a5c56580e7ed7e7291e
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-04-01 10:04:23 -0400

    refactoring

--- refactoring.py
+++ refactoring.py
@@ -5,10 +5,10 @@ def calculate(d):
 
 calculate(5)   # 78.5
 
-# The namse of the variables above don't give the readers much information and although there are not
+# The names of the variables above don't give the readers much information and although there are not
 # clues that result might used for.
 
-# In programming languag, when we rewrite code to be more self documenting, we call this process 
+# In programming language, when we rewrite code to be more self documenting, we call this process 
 # refactoring. The refactoring of the above code is:
 
 def circle_area(radius):
@@ -21,3 +21,4 @@ circle_area(5)
 # Now with refactoring, the namse of the variables and function, reflect the purpose, which helps the 
 # reader understand the code more quickly.
 
+# sometimes we can add a comment to our code
